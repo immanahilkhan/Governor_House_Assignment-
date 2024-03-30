26887270f263590b563fa25a4812864b35e0eb89
Manahil Khalid <hussainmanhilkhalid@gmail.com>
2024-03-30 22:38:28 +0500

    remove extra file

--- Name Cases Q3/app.js
+++
@@ -1,7 +0,0 @@
-// lowercase
-var personName = "Manahil";
-console.log("lowercase:", personName.toLowerCase());
-// uppercase
-console.log("uppercase:", personName.toUpperCase());
-// titlecase
-console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
--- Name Cases Q3/app.ts
+++
@@ -1,9 +0,0 @@
-// lowercase
-let personName: string ="Manahil"
-console.log("lowercase:", personName.toLowerCase());
-
-// uppercase
-console.log("uppercase:", personName.toUpperCase());
-
-// titlecase
-console.log("titlecase:", personName.replace(/\bw/g,c =>c.toUpperCase()));
